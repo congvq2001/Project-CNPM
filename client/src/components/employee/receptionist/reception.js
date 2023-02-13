33c1d972f09cfa610915c509851736a99db5e6a9
realhugn <125058050+hung20200294@users.noreply.github.com>
2023-02-14 04:19:29 +0700

    Fix api bán vé lẻ, checkin, out

--- client/src/components/employee/receptionist/reception.js
+++ client/src/components/employee/receptionist/reception.js
@@ -29,25 +29,29 @@ const ticket2 = {
 function Reception() {
   const [inTickets, setInTickets] = useState({});
   const [ticketList, setTicketList] = useState([]);
+  const [preId,setPreId] = useState("")
   const [idTicket, setIdTicket] = useState("");
+  const [cusId, setCusId] = useState("")
+  const [name, setName] = useState("")
+  const [phoneN ,setPhoneN] = useState("")
+  const [phone, setPhone] = useState("")
   const [quantity, setQuantity] = useState(1);
   const [demoP, setDemoP] = useState(0);
-  const [outprice, setoutprice] = useState(0);
-  const [VIPList, setVIPList] = useState([]);
-  const [phone, setphone] = useState("");
-  const [iduser, setiduser] = useState("");
+  const [found, setfound] = useState(false);
+  const [eventList, setEventList] = useState([]);
+  const [isPreOrder, setIsPreOrder] = useState(false);
   const [idTicketVIP, setIdTicketVIP] = useState("");
   const [hide, sethide] = useState(true);
-
+  const [checkoutId, setCheckoutId] = useState("")
   const [idUT, setidUT] = useState("");
   
   useEffect(() => {
-    fetch("http://localhost:5000/api/v1/ticket/61eaafd99cc06741fc0d4cda", {
+    fetch("http://localhost:5000/api/v1/ticket/", {
       method: 'GET',
     })
     .then(response => response.json())
-    .then(data => {
-      setTicketList(data.result.type);
+      .then(data => {
+    setTicketList(data.result);
     })
     .catch((error) => {
       alert("eror");
@@ -55,12 +59,12 @@ function Reception() {
   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/v1/ticket/61eae5e4ac7bee37e0362af5", {
+    fetch("http://localhost:5000/api/v1/eventTicket", {
       method: 'GET',
     })
     .then(response => response.json())
     .then(data => {
-      setVIPList(data.result.type);
+      setEventList(data.result);
     })
     .catch((error) => {
       alert("eror");
@@ -70,29 +74,36 @@ function Reception() {
   useEffect(() => {
     ticketList.map(tk=>{
         if(tk._id===idTicket){
-            setDemoP(tk.price*quantity);
+            setDemoP(isPreOrder ? tk.price*quantity*0.8 :  tk.price*quantity);
             return;
         }
     })
-  }, [quantity,idTicket]);
-
+  }, [quantity, idTicket]);
+  
   const handleSubmit= async (e)=>{
     e.preventDefault();
     if(idTicket&&quantity){
     try{ 
-      let time_checkin=new Date();
-      let res=await axios.put("http://localhost:5000/api/v1/staff/checkin",
+  
+      let res= cusId ? await axios.put("http://localhost:5000/api/v1/staff/checkin",
       {
-        id_ticket:idTicket,
-        price:demoP,
-        is_paid:false,
-        time_checkin
+        ticketId: idTicket,
+        cusId,
+        cusPhone: phone,
+        cusName: name,
+        quantity
+      },) : await axios.put("http://localhost:5000/api/v1/staff/checkin",
+      {
+        ticketId: idTicket,
+        cusPhone: phone,
+        cusName: name,
+        quantity
       },)
       if(res.data.status){
-        alert("tạo vé thành công")
-        setidUT(res.data.id)
+        alert("check in thành công")
+        setidUT(res.data.data)
       }else{
-        alert("tạo vé thất bại")
+        alert("check in thất bại")
       }
     }catch(err){
         alert("err")
@@ -102,49 +113,23 @@ function Reception() {
 
   const handleFindIn=async (e)=>{
     e.preventDefault();
-    if(idUT){
       try{
-        const res=await axios.get(`http://localhost:5000/api/v1/userTicket/${idUT}`)
+        const res=await axios.get(`http://localhost:5000/api/v1/userTicket/${checkoutId}`)
         setInTickets(res.data.result)
       }catch(err){
         alert("err")
       }
     }
-  }
 
-  const handleCheckin=async(e)=>{
-    e.preventDefault();
-    try{
-      let time_checkin=new Date();
-      const res=await axios.put("http://localhost:5000/api/v1/staff/checkin",{
-        idUserTicket:inTickets._id,
-        is_paid:false,
-        time_checkin
-      })
-      
-      if(res.data.status){
-        alert("Checkin thành công")
-      }else{
-        alert("Checkin thất bại")
-      }
-    }catch(err){
-      alert("err")
-    }
-  }
 
   const handleCheckout=async(e)=>{
     e.preventDefault();
     try{
-      let time_checkout=new Date();
-      const res=await axios.put("http://localhost:5000/api/v1/staff/checkout",{
-        idUserTicket:inTickets._id,
-        is_paid:true,
-        time_checkout
-      })
+      const res=await axios.put(`http://localhost:5000/api/v1/staff/checkout/${checkoutId}`)
       console.log(res)
       if(res.data.status){
         alert("Checkout thành công")
-        setoutprice(res.data.priceTicket)
+        setInTickets(res.data.checkedTicket)
       }else{
         alert("Checkout thất bại")
       }
@@ -153,76 +138,64 @@ function Reception() {
     }
   }
 
-  const checkloaive=(id)=>{
-    let name=""
-    ticketList.map(iteam=>{
-      if(iteam._id===id) return name=iteam.nameTicket;
-    })
-    return name;
+
+  // const getgiaVIP = (id)=>{
+  //   let gia=0;
+  //   VIPList.map(iteam=>{
+  //     if(iteam._id===id) gia=iteam.price;
+  //   })
+  //   return gia;
+  // }
+
+  const find=async(id)=>{
+    fetchiduser(id);
+    sethide(false);
   }
 
-  const getgiaVIP = (id)=>{
-    let gia=0;
-    VIPList.map(iteam=>{
-      if(iteam._id===id) gia=iteam.price;
-    })
-    return gia;
-  }
+  // const handleVIP =async(e)=>{
+  //   e.preventDefault();
+  //   try{
+  //     console.log(iduser)
+  //     console.log(idTicketVIP)
+  //     const res = await axios.post("http://localhost:5000/api/v1/ticket-vip",{
+  //       id_user: iduser,
+  //       id_ticket: idTicketVIP
+  //     })
+  //     console.log(res)
+  //     if(res.data.success){
+  //       alert("Đăng kí VIP thành công")
+  //       buyVIP();
+  //     }else alert("thất bại")
+  //   }catch(err){
+  //     alert("err")
+  //   }
+  // }
 
-  const find=async()=>{
-    fetchiduser();
-  }
-
-  const handleVIP =async(e)=>{
-    e.preventDefault();
-    try{
-      console.log(iduser)
-      console.log(idTicketVIP)
-      const res = await axios.post("http://localhost:5000/api/v1/ticket-vip",{
-        id_user: iduser,
-        id_ticket: idTicketVIP
-      })
-      console.log(res)
-      if(res.data.success){
-        alert("Đăng kí VIP thành công")
-        buyVIP();
-      }else alert("thất bại")
-    }catch(err){
-      alert("err")
-    }
-  }
-
-  const fetchiduser=async()=>{
-    try{
-      const res=await axios.get(`http://localhost:5000/api/v1/iduser/${phone}`)
-      if(res.data.success){
-        setiduser(res.data.data)
-        alert(`tìm kiếm thành công ${res.data.data}`)
-        sethide(false);
-      }else{
+  const fetchiduser = async (id) => {
+    try {
+      const res = await axios.post(`http://localhost:5000/api/v1/staff/searchByPhone/${id}`, { phone: phone })
+      if (res.data.success == 1) {
+        setCusId(res.data.result.cusId._id)
+        alert(`tìm kiếm thành công ${res.data.result.cusId._id}`)
+        setName(res.data.result.cusId.name)
+        setIsPreOrder(true)
+        setfound(true)
+        setQuantity(res.data.result.quantity)
+      } else if(res.data.success == 2) {
+        setCusId(res.data.result._id)
+        alert(`người dùng ${res.data.result._id} chưa đặt vé trước`)
+        setName(res.data.result.name)
+        setfound(true)
+        setIsPreOrder(false)
+      } else {
         alert("loi truy van")
       }
-    }catch(err){
+    } catch (err) {
+      setfound(false)
+      setCusId("")
+      setName("")
+      console.log(err)
       alert("err")
-    }
-  }
-
-  const buyVIP=async()=>{
-    try{
-      const time_checkout=new Date()
-      const rp=axios.post("http://localhost:5000/api/v1/userbuyticketwithouttoken",{
-        id_user:iduser,
-        id_ticket:idTicketVIP,
-        price:getgiaVIP(idTicketVIP),
-        time_checkout
-      })
-      if(rp.data.success){
-        alert("them vao database thanh cong")
-      }else{
-        alert("that bai")
-      }
-    }catch(err){
-      console.log("er")
     }
   }
 
@@ -232,62 +205,28 @@ function Reception() {
   backgroundImage: 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)'}}>
     <div className='reception1'>
       <Tabs fill defaultActiveKey='1'>
-
-
-
-
-        <Tab eventKey='1' title='Check in' className='tab'>
-        <div className='search-bar'>
-          <InputGroup>
-          <Form.Control placeholder='vd. id user_ticket' onChange={e=>setidUT(e.target.value)}/>
-          <Button onClick={(e) => handleFindIn(e)}>Tìm vé</Button>
-          </InputGroup>
-        </div>
-          <hr/>
-          <div className='ticket'>
-            {
-              
-                (inTickets&&(!inTickets.time_checkin)&&(!inTickets.time_checkout))
-                ?
-                 
-                    <Card>
-                      <div className='ticket'>
-                        <p>Loại vé: {checkloaive(inTickets.id_ticket)}</p>
-                        <p>Số lượng: {inTickets.quantity} vé</p>
-                        <p>Số tiền dự kiến: {inTickets.price} vnđ</p>
-                        <Button onClick={(e)=>handleCheckin(e)}>Check in</Button>
-                      </div>
-                    </Card>
-                  :
-                  <>
-                  </>
-              }
-          </div>
-        </Tab>
-
-
-
-
         <Tab eventKey='2' title='Check out' className='tab'>
         <div className='search-bar'>
           <InputGroup>
-          <Form.Control placeholder='vd. id user_ticket' onChange={e=>setidUT(e.target.value)}/>
-          <Button onClick={(e) => handleFindIn(e)}>Tìm vé</Button>
+          <Form.Control placeholder='vd. id user_ticket' onChange={e=>setCheckoutId(e.target.value)}/>
+          <Button onClick={(e)=>handleCheckout(e)}>Check out</Button>
           </InputGroup>
         </div>
           <hr/>
           <div className='ticket'>
             {
-              
-                (inTickets&&(inTickets.time_checkin)&&(!inTickets.time_checkout))
+                (inTickets&&(inTickets.createdAt)&&(inTickets.status == -1))
                 ?
-                 
                     <Card>
                       <div className='ticket'>
-                        <p>Loại vé: {checkloaive(inTickets.id_ticket)}</p>
+                        <p>Tên vé: {inTickets.name}</p>
                         <p>Số lượng: {inTickets.quantity} vé</p>
-                        <p>Số tiền dự kiến: {inTickets.price} vnđ</p>
-                        <Button onClick={(e)=>handleCheckout(e)}>Check out</Button>
+                        <p>Tên khách hàng:  {inTickets.cusName}</p>
+                        <p>Số điện thoại :  {inTickets.cusPhone}</p>
+                        <p>Tiền trả thêm : {inTickets.overPrice}</p>
+                        <p>Tiền đã trả :  {inTickets.price}</p>
+                        <p>Thời gian check in: {inTickets.createdAt}</p>
+                        <p>Số tiền phải trả: {inTickets.price + inTickets.overPrice} vnđ</p>
                       </div>
                       <></>
                     </Card>
@@ -296,66 +235,48 @@ function Reception() {
                   </>
               }
           </div>
-          <h5 style={{marginTop:"20px"}}>Số tiền phải trả:</h5>
-          <p>{outprice}</p>
         </Tab>
-
-
-
-        <Tab eventKey='3' title='Tạo vé mới' className='tab'>
+        <Tab eventKey='1' title='Check In' className='tab'>
           <Form>
-      
             <Form.Label>Loại vé{star}</Form.Label>
             <Form.Control as='select' onChange={e=>setIdTicket(e.target.value)}>
               <option>Chọn loại vé</option>
               {
                 ticketList.map((option) => 
-                  <option value={option._id}>{option.nameTicket}</option>
+                  <option value={option._id}>{option.name}</option>
                 )
               }
             </Form.Control>
-            <Form.Label>Số lượng{star}</Form.Label>
-            <Form.Control onChange={e=>setQuantity(e.target.value) } type="number" defaultValue="1" min="1"/>
-            <hr/>
+            <Form.Label>Số điện thoại {star}</Form.Label>
+            <Form.Control onChange={e => setPhone(e.target.value)} type="phone" />
+            <br/>
+            <Button onClick={(e) => find(idTicket)}>Tìm kiếm</Button>
+            <br/>
+            {!hide ? 
+              <>
+                <Form.Label>Số lượng</Form.Label>
+                <Form.Control type="number" value={quantity} min="1" disabled />
+                <Form.Label>Tên {star}</Form.Label>
+                <Form.Control onChange={e => setName(e.target.value)} type="name" disabled={found ? true : false} value={name} />
+                <Form.Label>Id nguời dùng</Form.Label>
+                <Form.Control onChange={e => setCusId(e.target.value)} type="text" disabled={found ? true : false} value={cusId} /> 
+                <hr />
+              </> : <></>}
             <p>Giá tiền thanh toán: {demoP}vnđ</p>
             <Button onClick={(e) => handleSubmit(e)}>Xác nhận</Button>
           </Form>
           <h5 style={{marginTop:"20px"}}>Vé được tạo:</h5>
-          <p>{idUT}</p>
-        </Tab>
-
-
-
-        <Tab eventKey='4' title='Đăng ký vip' className='tab'>
-          <Form>
-            <Form.Label>Số điện thoại{star}</Form.Label>
-            <Form.Control placeholder='vd. 0987654321' onChange={e=>setphone(e.target.value)}/>
-            <div style={{marginTop:"10px",marginBottom:"5px"}}>
-            <Button onClick={(e)=>find(e)}>Tìm kiếm</Button>
-            </div>
-           
-            <Form.Label>Gói VIP{star}</Form.Label>
-            <Form.Control as='select' onChange={e=>setIdTicketVIP(e.target.value)}>
-              <option>Chọn gói VIP</option>
-              {
-                VIPList.map(iteam=>{
-                  return(
-                    <option key={iteam._id} value={iteam._id}>{iteam.time} {iteam.kindOfTime}</option>
-                  )
-                })
-              }
-            </Form.Control>
-            <hr/>
-            <p>Giá tiền: {getgiaVIP(idTicketVIP)} vnđ</p>
-            {
-              hide
-              ?
-              <></>
-              :
-              <Button onClick={(e)=>handleVIP(e)}>Xác nhận</Button>
-            }
-            
-          </Form>
+        { idUT ? <div>
+            Id Vé: {idUT._id} <br />
+            Tên vé: {idUT.ticketId.name} <br />
+            Tên khách hàng:{idUT.cusName} <br />
+            Số điện thoại: {idUT.phone} <br />
+            Vip: {idUT.isVip ? 'Có' : 'Không'} <br />
+            Đặt trước: {idUT.isPreOrder ? 'Có' : 'Không'} <br />
+            Đã thanh toán: {idUT.price} <br />
+            Số lượng: {idUT.quantity} <br />
+            Thời gian check in:{idUT.createdAt.split('T')[0]} <br />
+          </div> : <></>}
         </Tab>
       </Tabs>
     </div>
